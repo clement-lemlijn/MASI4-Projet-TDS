fb203151b44f30972b3788d5c1b5c1085b09ff4b
LEMLIJN Clément <clement.lemlijn@gmail.com>
2026-04-30 10:35:53 +0200

    preparing stubs

---
+++ src/main/java/domain/image/processing/lineaire/FiltrageLinaireGlobal.java
@@ -0,0 +1,20 @@
+package domain.image.processing.lineaire;
+
+import jdk.jshell.spi.ExecutionControl;
+
+public class FiltrageLinaireGlobal {
+
+    public static int[][] filtrePasseBasIdeal(int[][] image,int frequenceCoupure){
+        return null;
+    }
+    public static int[][] filtrePasseHautIdeal(int[][] image,int frequenceCoupure){
+        return null;
+    }
+    public static int[][] filtrePasseBasButterworth(int[][] image,int frequenceCoupure,int ordre){
+        return null;
+    }
+    public static int[][] filtrePasseHautButterworth(int[][] image,int frequenceCoupure, int ordre) {
+        return null;
+    }
+
+}
